c882917d63cc1988e4ecb29a37fffe683619242b
SolTTeucas <122246568+SolTTeucas@users.noreply.github.com>
2024-12-25 20:28:24 +0700

    Create 03_guided.go

---
+++ GITHUB-REPO-BARU/03_tipe-data-dan-variabel_latihan1/guided/03_guided.go
@@ -0,0 +1,39 @@
+// //MODUL 3 SOAL 1
+// //PROGRAM UNTUK MENGHITUNG VOLUME KUBUS DARI PANJANG SISI
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var sisi, volume float64
+// 	fmt.Scan(&sisi)
+// 	volume = sisi * sisi * sisi
+// 	fmt.Println(volume)
+// }
+
+// MODUL 3 SOAL 2
+// PROGRAM UNTUK MENGHITUNG LUAS SEGITIGA BERDASARKAN ALAS DAN TINGGI
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var alas, tinggi, luas float64
+// 	fmt.Scan(&alas, &tinggi)
+// 	luas = 0.5 * alas * tinggi
+// 	fmt.Println(luas)
+// }
+
+// MODUL 3 SOAL 3
+// PROGRAM UNTUK MENGHITUNG KONVERSI UANG IDR KE USD
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var rupiah, dolar int
+// 	fmt.Scan(&rupiah)
+// 	dolar = rupiah / 15000
+// 	fmt.Println(dolar)
+// }
