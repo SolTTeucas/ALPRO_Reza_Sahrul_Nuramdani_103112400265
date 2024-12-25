a6521092f2bc41319a25a55fd2ba6d02f2c2c138
SolTTeucas <122246568+SolTTeucas@users.noreply.github.com>
2024-12-25 20:43:32 +0700

    Create 08_guided.go

---
+++ GITHUB-REPO-BARU/08_if-then/guided/08_guided.go
@@ -0,0 +1,64 @@
+//soalPertama
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var angka int
+
+// 	// Meminta pengguna memasukkan sebuah bilangan bulat
+// 	fmt.Print("Masukkan sebuah bilangan bulat: ")
+// 	fmt.Scan(&angka)
+
+// 	// Menghitung nilai absolut
+// 	if angka < 0 {
+// 		angka = -angka
+// 	}
+
+// 	// Menampilkan nilai absolut
+// 	fmt.Printf("Nilai absolut: %d\n", angka)
+// }
+
+//soalKedua
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var angka int
+
+// 	// Meminta pengguna memasukkan sebuah angka
+// 	fmt.Print("Masukkan sebuah angka: ")
+// 	fmt.Scan(&angka)
+
+// 	if angka > 0 {
+// 		fmt.Println("positif.")
+// 	} else {
+// 		fmt.Println("negatif.")
+// 	}
+// }
+
+//soalKetiga
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var angka int
+
+// 	// Meminta pengguna memasukkan sebuah angka
+// 	fmt.Print("Masukkan sebuah angka: ")
+// 	fmt.Scan(&angka)
+
+// 	if angka < 0 && angka%2 == 0 {
+// 		fmt.Println("true.")
+// 	} else {
+// 		fmt.Println("false.")
+// 	}
+// }
