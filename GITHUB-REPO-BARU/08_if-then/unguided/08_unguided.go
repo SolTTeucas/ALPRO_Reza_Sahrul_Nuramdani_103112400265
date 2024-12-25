899788a85e4a8a055df3b25592261eec05f97c96
SolTTeucas <122246568+SolTTeucas@users.noreply.github.com>
2024-12-25 20:41:01 +0700

    Create 08_unguided.go

---
+++ GITHUB-REPO-BARU/08_if-then/unguided/08_unguided.go
@@ -0,0 +1,74 @@
+//soalSatu
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var jumlahOrang int
+
+// 	fmt.Print("masukan jumlah orang yang akan mengikuti touring: ")
+// 	fmt.Scan(&jumlahOrang)
+
+// 	var jumlahMotor int
+// 	if jumlahOrang%2 == 0 {
+// 		jumlahMotor = (jumlahOrang / 2) 
+// 	} else {
+// 		jumlahMotor = (jumlahOrang / 2) + 1
+// 	}
+
+// 	fmt.Println("jumlah motor yang dibutuhkan: ", jumlahMotor)
+// }
+
+//soalDua
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var angka int
+
+// 	// Meminta pengguna memasukkan sebuah angka
+// 	fmt.Print("Masukkan sebuah angka: ")
+// 	fmt.Scan(&angka)
+
+// 	if angka < 0 && angka%2 == 0 {
+// 		fmt.Println("genap negatif.")
+// 	} else {
+// 		fmt.Println("bukan.")
+
+//soalTiga
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var x, y int
+
+// 	// Meminta pengguna memasukkan dua bilangan bulat positif
+// 	fmt.Print("Masukkan bilangan pertama (x): ")
+// 	fmt.Scan(&x)
+// 	fmt.Print("Masukkan bilangan kedua (y): ")
+// 	fmt.Scan(&y)
+
+// 	// Mengecek apakah x adalah faktor dari y
+// 	if y%x == 0 {
+// 		fmt.Printf("Apakah %d adalah faktor dari %d? true\n", x, y)
+// 	} else {
+// 		fmt.Printf("Apakah %d adalah faktor dari %d? false\n", x, y)
+// 	}
+
+// 	// Mengecek apakah y adalah faktor dari x
+// 	if x%y == 0 {
+// 		fmt.Printf("Apakah %d adalah faktor dari %d? true\n", y, x)
+// 	} else {
+// 		fmt.Printf("Apakah %d adalah faktor dari %d? false\n", y, x)
+// 	}
+// }
+// 	}
+// }
