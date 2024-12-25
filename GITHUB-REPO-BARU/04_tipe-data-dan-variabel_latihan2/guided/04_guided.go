ceaa5a9d1aa804e89272fef127ceff56f76b752c
SolTTeucas <122246568+SolTTeucas@users.noreply.github.com>
2024-12-25 20:32:49 +0700

    Create 04_guided.go

---
+++ GITHUB-REPO-BARU/04_tipe-data-dan-variabel_latihan2/guided/04_guided.go
@@ -0,0 +1,42 @@
+// MODUL 4 SOAL 1
+// PROGRAM UNTUK MENGKONVERSI DETIKKE JAM, MENIT DAN DETIK
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var detik, jam, menit int
+// 	fmt.Scan(&detik)
+// 	jam = detik / 3600
+// 	menit = (detik % 3600) / 60
+// 	detik = detik % 60
+// 	fmt.Println(jam, "jam", menit, "menit dan", detik, "detik")
+// }
+
+// MODUL 4 SOAL 2
+// PROGRAM UNTUK MENENTUKAN JIKA SETIAP DIGIT TERURUT MEMBESAR ATAU TIDAK
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var bilangan, d1, d2, d3 int
+// 	fmt.Scan(&bilangan)
+// 	d1 = bilangan / 100
+// 	d2 = bilangan % 100 / 10
+// 	d3 = bilangan % 100 % 10
+// 	fmt.Println(d1 <= d2 && d2 <= d3)
+// }
+
+// MODUL 4 SOAL 3
+// PROGRAM UNTUK MENGHITUNG BMI ATAU BODY INDESK MASS
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var beratBadan, tinggiBadan, bmi float64
+// 	fmt.Scan(&beratBadan, &tinggiBadan)
+// 	bmi = beratBadan / (tinggiBadan * tinggiBadan)
+// 	fmt.Printf("%.2f", bmi)
+// }
