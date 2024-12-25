dffca6e262fc3cd54e50e17f1b76f4021a46f9fb
SolTTeucas <122246568+SolTTeucas@users.noreply.github.com>
2024-12-25 20:35:29 +0700

    Create 05_guided.go

---
+++ GITHUB-REPO-BARU/05_06_for-loop/guided/05_guided.go
@@ -0,0 +1,49 @@
+// MODUL 5 SOAL 1
+// PROGRAM BARIS BILANGAN A SAMPAI B
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var a, b int
+// 	var j int
+// 	fmt.Scan(&a, &b)
+// 	for j = a; j <= b; j += 1 {
+// 		fmt.Print(j, " ")
+// 	}
+// }
+
+// MODUL 5 SOAL 2
+// PROGRAM LUAS N SEGITIGA
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var j, alas, tinggi, n int
+// 	var luas float64
+// 	fmt.Scan(&n)
+// 	for j = 1; j <= n; j += 1 {
+// 		fmt.Scan(&alas, &tinggi)
+// 		luas = 0.5 * float64(alas*tinggi) //rumus luas segitiga yang digunakan pada input variables
+// 		fmt.Println(luas)
+// 	}
+// }
+
+// MODUL 5 SOAL 3
+// PROGRAM PERKALIAN TANPA OPERATOR "*"
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var j, v1, v2 int
+// 	var hasil int
+// 	fmt.Scan(&v1, &v2)
+// 	hasil = 0
+// 	for j = 1; j <= v2; j += 1 {
+// 		hasil = hasil + v1
+// 	}
+// 	fmt.Println(hasil)
+// }
